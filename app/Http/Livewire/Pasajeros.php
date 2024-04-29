3e78919d6fd46525beac0fd9a8273cc6029580f8
Agustin Aguero <agustin.aguero@gmail.com>
2024-04-29 14:40:52 -0400

    creacion de la migracion, facotry, seed y componentes livewire de triuplanes y pasajeros

---
+++ app/Http/Livewire/Pasajeros.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Pasajeros extends Component
+{
+    public function render()
+    {
+        return view('livewire.pasajeros');
+    }
+}
