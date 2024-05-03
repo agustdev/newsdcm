72d61bcec07853c158f61a198db71999c87f8539
danielzt8 <nikolazt98@gmail.com>
2024-05-02 21:31:23 -0400

    cambios usuario

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,9 +26,9 @@ class UserFactory extends Factory
     {
         return [
             'name' => $this->faker->name(),
-            'email' => 'agustin.aguero@gmail.com',
+            'email' => 'nikolazt98@gmail.com',
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => bcrypt('holamundo123'), // password
             'two_factor_secret' => null,
             'two_factor_recovery_codes' => null,
             'remember_token' => Str::random(10),
