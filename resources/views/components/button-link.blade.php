262534d990f8f2a7c6e7da9a319235c4e06cb80b
danielzt8 <nikolazt98@gmail.com>
2024-05-04 01:17:43 -0400

    cambie de color el boton de registro

--- resources/views/components/button-link.blade.php
+++ resources/views/components/button-link.blade.php
@@ -1,7 +1,7 @@
 <a
     {{ $attributes->merge([
-        'class' => 'inline-flex items-center px-8 py-2 bg-red-500 border
-                                border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-700
+        'class' => 'inline-flex items-center px-8 py-2 bg-yellow-500 border
+                                border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-yellow-600
                                 focus:bg-red-700 active:bg-red-900 focus:outline-none focus:ring-2 focus:ring-red-400 focus:ring-offset-2
                                 transition ease-in-out duration-150 ml-1',
     ]) }}>
