284270ccac1b6050d6e1720780dbbc8cda3de052
danielzt8 <nikolazt98@gmail.com>
2024-05-05 11:37:54 -0400

    ajuste los colores active y focus

--- resources/views/components/button-link.blade.php
+++ resources/views/components/button-link.blade.php
@@ -2,7 +2,7 @@
     {{ $attributes->merge([
         'class' => 'inline-flex items-center px-8 py-2 bg-yellow-500 border
                                 border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-yellow-600
-                                focus:bg-red-700 active:bg-red-900 focus:outline-none focus:ring-2 focus:ring-red-400 focus:ring-offset-2
+                                focus:bg-yellow-700 active:bg-yellow-800 focus:outline-none focus:ring-2 focus:ring-yellow-400 focus:ring-offset-2
                                 transition ease-in-out duration-150 ml-1',
     ]) }}>
     {{ $slot }}
