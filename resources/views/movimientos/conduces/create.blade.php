f5bf691415bf311af10d8a53a2da83759b869dd0
Agustin Aguero <agustin.aguero@gmail.com>
2024-05-10 09:32:54 -0400

    fix: arreglo inicial formularios
    queda revisar conflictos de las versiones

--- resources/views/movimientos/conduces/create.blade.php
+++ resources/views/movimientos/conduces/create.blade.php
@@ -1,10 +1,10 @@
 <x-app-layout>
-    @section('titulo', 'Solicitud de Conduces')
+    @section('titulo', __('Solicitud de Conduces'))
     @push('css')
         <link href="{{ asset('css/custom.css') }}" rel="stylesheet" type="text/css" />
     @endpush
     <x-slot name="header">
-        <h2 class="h2 mb-3 mt-2">
+        <h2 class="h2 mb-3 mt-2 text-center">
             {{ __('Solicitud de Conduce') }}
         </h2>
     </x-slot>
@@ -13,36 +13,45 @@
 
         <form action="{{ route('movimientos.conduces.store') }}" method="POST" class="form-inline" autocomplete="off">
             @csrf
-            <div class="card">
-                <div class="card-header">
+            {{-- primera tarjeta --}}
+            <div class="card shadow-xl">
+                <div class="card-body">
                     <div class="col-lg-12 mb-2">
-                        <h3 class="h4 uppercase">{{ __('Número de solicitud') }}: {{ empty($ultimo_mov) ? 1 : $ultimo_mov->id + 1 }}
+                        <h3 class="h4 uppercase">{{ __('Número de solicitud') }}:
+                            {{ empty($ultimo_mov) ? 1 : $ultimo_mov->id + 1 }}
                         </h3>
+                    </div>
+                </div>
+            </div>
+            {{-- primera tarjeta tarjeta --}}
+
+            {{-- informacion de la embarcacion --}}
+            <div class="card shadow-xl">
+                <div class="card-header bg-blue-900">
+                    <div class="text-white">
+                        <strong>{{ __('INFORMACIÓN DE LA EMBARCACIÓN') }}</strong>
+                        <div role="status" class="spin-matricula float-end hidden">
+                            <svg aria-hidden="true" class="w-8 h-8 mr-2 text-gray-200 animate-spin fill-white"
+                                viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                                    fill="currentColor" />
+                                <path
+                                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                                    fill="currentFill" />
+                            </svg>
+                            <span class="sr-only">Loading...</span>
+                        </div>
                     </div>
                 </div>
                 <div class="card-body">
                     <div class="row g-2">
-                        <div class="alert alert-warning" role="alert">
-                            <strong>{{ __('INFORMACIÓN DE LA EMBARCACIÓN')}}</strong>
-                            <div role="status" class="spin-matricula float-end hidden">
-                                <svg aria-hidden="true"
-                                    class="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-700"
-                                    viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path
-                                        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                                        fill="currentColor" />
-                                    <path
-                                        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                                        fill="currentFill" />
-                                </svg>
-                                <span class="sr-only">Loading...</span>
-                            </div>
-                        </div>
                         <div class="col-md">
                             <div class="form-floating mb-2">
                                 {{-- <input type="text" class="form-control matricula" id="floatinMatricula"
                                     placeholder="MATRICULA" name="matricula" value="" required /> --}}
-                                <select name="matricula" class="form-select emb_matricula" id="floatinMatricula">
+                                <select name="matricula" class="form-select emb_matricula rounded-md"
+                                    id="floatinMatricula">
                                     @if ($embarcaciones->count() > 0)
                                         <option value="">- {{ __('Seleccione') }} -</option>
                                         @foreach ($embarcaciones as $embarcacion)
@@ -58,54 +67,54 @@
                         </div>
                         <div class="col-md">
                             <div class="form-floating mb-2">
-                                <input type="text" class="form-control nombre_emb" id="floatingNombreEmbarcacion"
-                                    placeholder="NOMBRE DE LA EMBARCACIÓN" name="nombre" readonly value=""
-                                    required />
+                                <input type="text" class="form-control nombre_emb rounded-md"
+                                    id="floatingNombreEmbarcacion" placeholder="NOMBRE DE LA EMBARCACIÓN" name="nombre"
+                                    readonly value="" required />
                                 <label for="floatingNombreEmbarcacion">{{ __('NOMBRE DE LA EMBARCACIÓN') }}</label>
                             </div>
                         </div>
 
                         <div class="col-md">
                             <div class="form-floating mb-3">
-                                <input type="text" class="form-control numero_casco" id="floatingNumeroCasco"
-                                    placeholder="NUMERO DE CASCO" name="numero_casco" readonly value=""
-                                    required />
-                                <label for="floatingNumeroCasco">{{ __('NUMERO DE CASCO')}}</label>
+                                <input type="text" class="form-control numero_casco rounded-md"
+                                    id="floatingNumeroCasco" placeholder="NUMERO DE CASCO" name="numero_casco" readonly
+                                    value="" required />
+                                <label for="floatingNumeroCasco">{{ __('NUMERO DE CASCO') }}</label>
                             </div>
                         </div>
                         <div class="col-md">
                             <div class="form-floating">
-                                <input type="text" class="form-control color_emb" id="floatingColor"
+                                <input type="text" class="form-control color_emb rounded-md" id="floatingColor"
                                     placeholder="COLOR DE LA EMBARCACIÓN" readonly name="color_emb" value=""
                                     required />
                                 <label for="floatingColor">{{ __('COLOR') }}</label>
                             </div>
                         </div>
-                        <div class="row mt-2">
-                            <span
+                        <div class="grid grid-cols-1 md:grid-cols-3 gap-2 mt-3">
+                            {{-- <span
                                 class="uppercase bg-gray-100 text-gray-600 text-sm font-medium mr-2 px-2.5 py-1.5 mb-1 rounded dark:bg-gray-700 dark:text-gray-300">
                                 {{ __('INFORMACIÓN DEL MOTOR DE LA EMBARCACIÓN') }}
-                            </span>
-                            <div class="col-md mb-2">
+                            </span> --}}
+                            <div class="mb-2">
                                 <div class="form-floating">
-                                    <input type="text" class="form-control marca_motor" id="floatingColor"
+                                    <input type="text" class="form-control marca_motor rounded-md" id="floatingColor"
                                         placeholder="MARCA MOTOR DE LA EMBARCACIÓN" readonly
                                         name="marca_modelo_motor" />
                                     <label for="floatingColor">{{ __('MARCA') }}</label>
                                 </div>
                             </div>
-                            <div class="col-md mb-2">
+                            <div class="mb-2">
                                 <div class="form-floating">
-                                    <input type="text" class="form-control caballos_motor" id="floatingColor"
-                                        placeholder="CABALLOS DE FUERZA MOTOR DE LA EMBARCACIÓN" readonly
-                                        name="caballos_fuerza_motor" />
+                                    <input type="text" class="form-control caballos_motor rounded-md"
+                                        id="floatingColor" placeholder="CABALLOS DE FUERZA MOTOR DE LA EMBARCACIÓN"
+                                        readonly name="caballos_fuerza_motor" />
                                     <label for="floatingColor">{{ __('CABALLOS DE FUERZA') }}</label>
                                 </div>
                             </div>
-                            <div class="col-md">
+                            <div class="">
                                 <div class="form-floating">
-                                    <input type="text" class="form-control numero_motor" id="floatingColor"
-                                        placeholder="NUMERO DE MOTOR" readonly name="no_motor" />
+                                    <input type="text" class="form-control numero_motor rounded-md"
+                                        id="floatingColor" placeholder="NUMERO DE MOTOR" readonly name="no_motor" />
                                     <label for="floatingColor">{{ __('NÚMERO DE MOTOR') }}</label>
                                 </div>
                             </div>
@@ -150,7 +159,8 @@
                                 <input type="text" class="form-control nombre_capitan"
                                     id="floatingNombreConductor" placeholder="NOMBRE Y APELLIDO DEL CONDUCTOR"
                                     value="" name="nombre_conductor" required />
-                                <label for="floatingNombreConductor">{{ __('NOMBRE Y APELLIDO DEL CONDUCTOR') }}R</label>
+                                <label
+                                    for="floatingNombreConductor">{{ __('NOMBRE Y APELLIDO DEL CONDUCTOR') }}R</label>
                             </div>
                         </div>
 
@@ -169,7 +179,8 @@
                                 <input type="text" class="form-control telefono2" id="floatingTelefono2Conductor"
                                     placeholder="OTRO TELÉFONO DEL CONDUCTOR" name="telefono_conductor_otro"
                                     value="" />
-                                <label for="floatingTelefono2Conductor">{{ __('OTRO TELÉFONO DEL CONDUCTOR') }}</label>
+                                <label
+                                    for="floatingTelefono2Conductor">{{ __('OTRO TELÉFONO DEL CONDUCTOR') }}</label>
                             </div>
                         </div>
                     </div>
@@ -257,7 +268,7 @@
                     <div class="row">
                         <span
                             class="bg-blue-100 text-blue-600 text-sm font-medium mr-2 px-2.5 py-1.5 mb-1 rounded dark:bg-blue-700 dark:text-blue-300">
-                            {{ __('LUGAR DESTINO')}}
+                            {{ __('LUGAR DESTINO') }}
                             <div role="status" class="spin float-end hidden">
                                 <svg aria-hidden="true"
                                     class="w-6 h-6 mr-2 text-gray-200 animate-spin dark:text-gray-200 fill-gray-700"
@@ -297,8 +308,8 @@
                         </div>
                         <div class="col-md">
                             <div class="form-floating mb-2">
-                                <input type="text" class="form-control" id="floatingSector" placeholder="{{ __('SECTOR') }}"
-                                    name="sector" />
+                                <input type="text" class="form-control" id="floatingSector"
+                                    placeholder="{{ __('SECTOR') }}" name="sector" />
                                 <label for="floatingSector">{{ __('SECTOR') }}</label>
                             </div>
                         </div>
@@ -307,8 +318,8 @@
 
                         <div class="col-md">
                             <div class="form-floating mb-2">
-                                <input type="text" class="form-control" id="floatingCalle" placeholder="{{ __('CALLE') }}"
-                                    name="calle" />
+                                <input type="text" class="form-control" id="floatingCalle"
+                                    placeholder="{{ __('CALLE') }}" name="calle" />
                                 <label for="floatingCalle">{{ __('CALLE') }}</label>
                             </div>
                         </div>
