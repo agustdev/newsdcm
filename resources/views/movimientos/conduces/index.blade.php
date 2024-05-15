15ab318d048d05ddce09c8e1aefe0c00d625e691
Daniel <nikolazt98@gmail.com>
2024-05-15 18:57:53 -0400

    boton azul cambiado al bg-primario y el color de hoover

--- resources/views/movimientos/conduces/index.blade.php
+++ resources/views/movimientos/conduces/index.blade.php
@@ -20,7 +20,7 @@
                     <h2 class="h3">
                         {{ __('Listado de Solicitudes de Conduce') }}
                         <a href="{{ route('movimientos.conduces.create') }}"
-                            class="inline-flex items-center justify-center px-3 py-2 bg-blue-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-500 active:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition ease-in-out duration-150 float-end"><i
+                            class="inline-flex items-center justify-center px-3 py-2 bg-azulito border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-600 active:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 transition ease-in-out duration-150 float-end"><i
                                 class="mdi mdi-plus mdi-18px"></i>{{ __('Nueva Solicitud') }}</a>
                     </h2>
                 </div>
