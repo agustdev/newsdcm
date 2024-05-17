e8aea649099dd83114caaede996d47f4783a38d5
Daniel <nikolazt98@gmail.com>
2024-05-16 22:41:21 -0400

    Cambie el color de los botones

--- resources/views/movimientos/despachos/create_post.blade.php
+++ resources/views/movimientos/despachos/create_post.blade.php
@@ -254,10 +254,10 @@
     <div class="card-footer">
         <div class="float-end">
             <a href="{{ route('movimientos.despachos.index') }}"
-                class="inline-flex items-center px-3 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150 ml-1">Atras</a>
+                class="inline-flex items-center px-3 py-2 bg- bg-slate-300 border border-transparent rounded-md font-semibold text-xs text-black uppercase tracking-widest hover:bg-slate-400 focus:bg-slate-500 active:bg-slate-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150 ml-1">Atras</a>
             <button type="submit"
-                class="inline-flex items-center justify-center px-4 py-2 bg-green-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-green-500 active:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 transition ease-in-out duration-150"><i
-                    class="mdi mdi-send mr-2"></i> Enviar</button>
+                class="inline-flex items-center justify-center px-4 py-2 bg-azulito border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-700 active:bg-blue-800 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:ring-offset-2 transition ease-in-out duration-150 disabled:opacity-25 send"> Enviar<i
+                    class="mdi mdi-send ml-2"></i></button>
         </div>
     </div>
 </div>
