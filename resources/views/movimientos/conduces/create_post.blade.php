6db72a674b1ae1379eeeee07c0d07218850a5864
Daniel <nikolazt98@gmail.com>
2024-05-17 17:44:39 -0400

    movi el lugar de destino a un nuevo card y arregle los estilos del card para que quede acorde con los estilos

--- resources/views/movimientos/conduces/create_post.blade.php
+++ resources/views/movimientos/conduces/create_post.blade.php
@@ -88,73 +88,7 @@
                     </div>
                     
                    
-                    <div class="row">
-                        <span
-                            class="bg-blue-100 text-blue-600 text-sm font-medium mr-2 px-2.5 py-1.5 mb-1 rounded dark:bg-blue-700 dark:text-blue-300">
-                            LUGAR DESTINO
-                            <div role="status" class="spin float-end hidden">
-                                <svg aria-hidden="true"
-                                    class="w-6 h-6 mr-2 text-gray-200 animate-spin dark:text-gray-200 fill-gray-700"
-                                    viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path
-                                        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                                        fill="currentColor" />
-                                    <path
-                                        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                                        fill="currentFill" />
-                                </svg>
-                                <span class="sr-only">Loading...</span>
-                            </div>
-                        </span>
-
-                        <div class="col-md">
-                            <div class="form-floating mb-2">
-                                <select class="form-select" name="provincia" id="floatingSelectProvincia">
-                                    <option>- Seleccione -</option>
-                                    @foreach ($provincias as $prov)
-                                        <option value="{{ $prov->id }}|{{ $prov->descripcion }}">
-                                            {{ $prov->descripcion }}
-                                        </option>
-                                    @endforeach
-                                </select>
-                                <label for="floatingSelectProvincia">PROVINCIA</label>
-                            </div>
-                        </div>
-
-                        <div class="col-md">
-                            <div class="form-floating mb-2">
-                                <select class="form-select" name="municipio" id="floatingSelectMunicipio">
-                                    <option>- Seleccione -</option>
-                                </select>
-                                <label for="floatingSelectMunicipio">MUNICIPIO</label>
-                            </div>
-                        </div>
-                        <div class="col-md">
-                            <div class="form-floating mb-2">
-                                <input type="text" class="form-control" id="floatingSector" placeholder="SECTOR"
-                                    name="sector" />
-                                <label for="floatingSector">SECTOR</label>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md">
-                            <div class="form-floating mb-2">
-                                <input type="text" class="form-control" id="floatingCalle" placeholder="CALLE"
-                                    name="calle" />
-                                <label for="floatingCalle">CALLE</label>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md">
-                            <div class="form-floating mb-2">
-                                <input type="text" class="form-control" id="floatinObservacion"
-                                    placeholder="OBSERVACIÓN" name="observacion" />
-                                <label for="floatinObservacion">OBSERVACIÓN</label>
-                            </div>
-                        </div>
-                    </div>
+                    
                 </div>
                 <div class="card-footer">
                     <div class="float-end">
@@ -249,34 +183,34 @@
 {{-- termina informacion del telefono del otro conductor --}}
 
 {{-- datos de vehiculo --}}
-<div class="row">
+<div class="row g-2">
     <span class="text-sm font-semibold text-black py-1.5 mb-1 rounded">
         DATOS DEL VEHÍCULO</span>
     <div class="col-md">
-        <div class="form-floating mb-2">
+        <div class="form-floating">
             <input type="text" class="form-control marca rounded-md" id="floatingMarcaModelo"
                 placeholder="NOMBRE DE LA EMBARCACIÓN" name="marca" required />
             <label style="font-size: 10px;" for="floatingMarcaModelo">MARCA Y MODELO</label>
         </div>
     </div>
     <div class="col-md">
-        <div class="form-floating mb-2">
+        <div class="form-floating">
             <input type="text" class="form-control color rounded-md" id="floatinColor"
                 placeholder="COLOR" name="color" required />
             <label style="font-size: 10px;" for="floatinColor">COLOR</label>
         </div>
     </div>
     <div class="col-md">
-        <div class="form-floating mb-2">
+        <div class="form-floating">
             <input type="text" class="form-control year rounded-md" id="floatingYear" placeholder="AÑO"
                 name="year" />
             <label style="font-size: 10px;" for="floatingYear">AÑO</label>
         </div>
     </div>
     <div class="col-md">
-        <div class="form-floating mb-2">
+        <div class="form-floating">
             <input type="text" class="form-control placa rounded-md" id="floatingPlaca"
-                placeholder="PLACA" name="placa" />
+                placeholder="PLACA" name="placa"/>
             <label style="font-size: 10px;" for="floatingPlaca">PLACA</label>
         </div>
     </div>
@@ -310,7 +244,7 @@
 
 
     <div class="card-body">
-        <div class="row">
+        <div class="row g-2">
             
             <div class="col-md">
                 <div class="form-floating mb-2">
@@ -352,7 +286,85 @@
 
 {{-- fin de lugar de salida --}}
 
+{{-- lugar de destino --}}
+<div class="card">
+    <div class="card-header bg-blue-900">
+        <span
+        class=" text-white text-sm font-bold">
+        LUGAR DESTINO
+        <div role="status" class="spin float-end hidden">
+            <svg aria-hidden="true"
+                class="w-6 h-6 mr-2 text-gray-200 animate-spin dark:text-gray-200 fill-gray-700"
+                viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                    fill="currentColor" />
+                <path
+                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                    fill="currentFill" />
+            </svg>
+            <span class="sr-only">Loading...</span>
+        </div>
+    </span>
+    </div>
 
+
+    <div class="card-body">
+        <div class="row g-2">
+            
+
+            <div class="col-md">
+                <div class="form-floating mb-2">
+                    <select class="form-select rounded-md" name="provincia" id="floatingSelectProvincia">
+                        <option>- Seleccione -</option>
+                        @foreach ($provincias as $prov)
+                            <option value="{{ $prov->id }}|{{ $prov->descripcion }}">
+                                {{ $prov->descripcion }}
+                            </option>
+                        @endforeach
+                    </select>
+                    <label style="font-size: 10px;" style="font-size: 10px;" for="floatingSelectProvincia">PROVINCIA</label>
+                </div>
+            </div>
+
+            <div class="col-md">
+                <div class="form-floating mb-2">
+                    <select class="form-select rounded-md" name="municipio" id="floatingSelectMunicipio">
+                        <option>- Seleccione -</option>
+                    </select>
+                    <label style="font-size: 10px;" style="font-size: 10px;" for="floatingSelectMunicipio">MUNICIPIO</label>
+                </div>
+            </div>
+            <div class="col-md">
+                <div class="form-floating mb-2">
+                    <input type="text" class="form-control rounded-md" id="floatingSector" placeholder="SECTOR"
+                        name="sector" />
+                    <label style="font-size: 10px;" style="font-size: 10px;" for="floatingSector">SECTOR</label>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md">
+                <div class="form-floating mb-2">
+                    <input type="text" class="form-control rounded-md" id="floatingCalle" placeholder="CALLE"
+                        name="calle" />
+                    <label style="font-size: 10px;" for="floatingCalle">CALLE</label>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md">
+                <div class="form-floating mb-2">
+                    <input type="text" class="form-control rounded-md" id="floatinObservacion"
+                        placeholder="OBSERVACIÓN" name="observacion" />
+                    <label style="font-size: 10px;" for="floatinObservacion">OBSERVACIÓN</label>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+{{-- fin de lugar de destino --}}
 
 
 
