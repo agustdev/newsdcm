e5111e31308318d79ea0760efc90fe7105af9391
Daniel <nikolazt98@gmail.com>
2024-05-17 16:54:03 -0400

    Movi la informacion del vehiculo a un nuevo card, junto con los datos del vehiculo

--- resources/views/movimientos/conduces/create_post.blade.php
+++ resources/views/movimientos/conduces/create_post.blade.php
@@ -86,100 +86,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="alert alert-info mt-2" role="alert">
-                            <strong>INFORMACIÓN DEL VEHÍCULO / CONDUCTOR / DESTINO</strong>
-                            <div role="status" class="spin-cap float-end hidden">
-                                <svg aria-hidden="true"
-                                    class="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-700"
-                                    viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path
-                                        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                                        fill="currentColor" />
-                                    <path
-                                        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                                        fill="currentFill" />
-                                </svg>
-                                <span class="sr-only">Loading...</span>
-                            </div>
-                        </div>
-                        <div class="col-md">
-                            <div class="form-floating mb-2">
-                                <select class="form-select tipo_documento" name="tipo_documento" id="floatingSelect">
-                                    <option>- Seleccione -</option>
-                                    <option value="cedula">Cédula</option>
-                                    <option value="pasaporte">Pasaporte</option>
-                                </select>
-                                <label for="floatinMatricula">TIPO DE DOCUMENTO</label>
-                            </div>
-                        </div>
-                        <div class="col-md">
-                            <div class="form-floating mb-2">
-                                <input type="text" class="form-control documento" id="floatinDocumento"
-                                    placeholder="Documento" name="documento" required />
-                                <label for="floatinDocumento">DOCUMENTO DE IDENTIDAD DEL CONDUCTOR</label>
-                            </div>
-                        </div>
-                        <div class="col-md">
-                            <div class="form-floating mb-2">
-                                <input type="text" class="form-control nombre_capitan"
-                                    id="floatingNombreConductor" placeholder="NOMBRE Y APELLIDO DEL CONDUCTOR"
-                                    value="" name="nombre_conductor" required />
-                                <label for="floatingNombreConductor">NOMBRE Y APELLIDO DEL CONDUCTOR</label>
-                            </div>
-                        </div>
-
-                        <div class="col-md">
-                            <div class="form-floating mb-2">
-                                <input type="text" class="form-control telefono1" id="floatingTelefonoConductor"
-                                    placeholder="TELEFONO CONDUCTOR" name="telefono_conductor" value=""
-                                    required />
-                                <label for="floatingTelefonoConductor">TELÉFONO DEL CONDUCTOR</label>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md">
-                            <div class="form-floating mb-2">
-                                <input type="text" class="form-control telefono2" id="floatingTelefono2Conductor"
-                                    placeholder="OTRO TELÉFONO DEL CONDUCTOR" name="telefono_conductor_otro"
-                                    value="" />
-                                <label for="floatingTelefono2Conductor">OTRO TELÉFONO DEL CONDUCTOR</label>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <span class="bg-gray-500  text-sm font-medium mr-2 px-2.5 py-1.5 mb-1 rounded  text-gray-300">
-                            DATOS DEL VEHÍCULO</span>
-                        <div class="col-md">
-                            <div class="form-floating mb-2">
-                                <input type="text" class="form-control marca" id="floatingMarcaModelo"
-                                    placeholder="NOMBRE DE LA EMBARCACIÓN" name="marca" required />
-                                <label for="floatingMarcaModelo">MARCA Y MODELO</label>
-                            </div>
-                        </div>
-                        <div class="col-md">
-                            <div class="form-floating mb-2">
-                                <input type="text" class="form-control color" id="floatinColor"
-                                    placeholder="COLOR" name="color" required />
-                                <label for="floatinColor">COLOR</label>
-                            </div>
-                        </div>
-                        <div class="col-md">
-                            <div class="form-floating mb-2">
-                                <input type="text" class="form-control year" id="floatingYear" placeholder="AÑO"
-                                    name="year" />
-                                <label for="floatingYear">AÑO</label>
-                            </div>
-                        </div>
-                        <div class="col-md">
-                            <div class="form-floating mb-2">
-                                <input type="text" class="form-control placa" id="floatingPlaca"
-                                    placeholder="PLACA" name="placa" />
-                                <label for="floatingPlaca">PLACA</label>
-                            </div>
-                        </div>
-                    </div>
+                    
                     <div class="row">
                         <span
                             class="bg-yellow-100 text-yellow-600 text-sm font-medium mr-2 px-2.5 py-1.5 mb-1 rounded dark:bg-yellow-700 dark:text-yellow-300">
@@ -307,6 +214,132 @@
                     </div>
                 </div>
             </div>
+
+
+
+
+
+
+
+
+
+<div class="card">
+    <div class="card-header bg-blue-900">
+        <div class="text-white mt-2 mb-2" role="alert">
+            <strong>INFORMACIÓN DEL VEHÍCULO / CONDUCTOR / DESTINO</strong>
+            <div role="status" class="spin-cap float-end hidden">
+                <svg aria-hidden="true"
+                    class="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-700"
+                    viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                        fill="currentColor" />
+                    <path
+                        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                        fill="currentFill" />
+                </svg>
+                <span class="sr-only">Loading...</span>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="card-body">
+{{-- informacion del vehiculo --}}
+<div class="row">
+    
+    <div class="col-md">
+        <div class="form-floating mb-2">
+            <select class="form-select tipo_documento" name="tipo_documento" id="floatingSelect">
+                <option>- Seleccione -</option>
+                <option value="cedula">Cédula</option>
+                <option value="pasaporte">Pasaporte</option>
+            </select>
+            <label for="floatinMatricula">TIPO DE DOCUMENTO</label>
+        </div>
+    </div>
+    <div class="col-md">
+        <div class="form-floating mb-2">
+            <input type="text" class="form-control documento" id="floatinDocumento"
+                placeholder="Documento" name="documento" required />
+            <label for="floatinDocumento">DOCUMENTO DE IDENTIDAD DEL CONDUCTOR</label>
+        </div>
+    </div>
+    <div class="col-md">
+        <div class="form-floating mb-2">
+            <input type="text" class="form-control nombre_capitan"
+                id="floatingNombreConductor" placeholder="NOMBRE Y APELLIDO DEL CONDUCTOR"
+                value="" name="nombre_conductor" required />
+            <label for="floatingNombreConductor">NOMBRE Y APELLIDO DEL CONDUCTOR</label>
+        </div>
+    </div>
+
+    <div class="col-md">
+        <div class="form-floating mb-2">
+            <input type="text" class="form-control telefono1" id="floatingTelefonoConductor"
+                placeholder="TELEFONO CONDUCTOR" name="telefono_conductor" value=""
+                required />
+            <label for="floatingTelefonoConductor">TELÉFONO DEL CONDUCTOR</label>
+        </div>
+    </div>
+</div>
+{{-- termina informacion del vehiculo --}}
+<div class="row">
+    <div class="col-md">
+        <div class="form-floating mb-2">
+            <input type="text" class="form-control telefono2" id="floatingTelefono2Conductor"
+                placeholder="OTRO TELÉFONO DEL CONDUCTOR" name="telefono_conductor_otro"
+                value="" />
+            <label for="floatingTelefono2Conductor">OTRO TELÉFONO DEL CONDUCTOR</label>
+        </div>
+    </div>
+</div>
+{{-- termina informacion del telefono del otro conductor --}}
+
+{{-- datos de vehiculo --}}
+<div class="row">
+    <span class="bg-gray-500  text-sm font-medium mr-2 px-2.5 py-1.5 mb-1 rounded  text-gray-300">
+        DATOS DEL VEHÍCULO</span>
+    <div class="col-md">
+        <div class="form-floating mb-2">
+            <input type="text" class="form-control marca" id="floatingMarcaModelo"
+                placeholder="NOMBRE DE LA EMBARCACIÓN" name="marca" required />
+            <label for="floatingMarcaModelo">MARCA Y MODELO</label>
+        </div>
+    </div>
+    <div class="col-md">
+        <div class="form-floating mb-2">
+            <input type="text" class="form-control color" id="floatinColor"
+                placeholder="COLOR" name="color" required />
+            <label for="floatinColor">COLOR</label>
+        </div>
+    </div>
+    <div class="col-md">
+        <div class="form-floating mb-2">
+            <input type="text" class="form-control year" id="floatingYear" placeholder="AÑO"
+                name="year" />
+            <label for="floatingYear">AÑO</label>
+        </div>
+    </div>
+    <div class="col-md">
+        <div class="form-floating mb-2">
+            <input type="text" class="form-control placa" id="floatingPlaca"
+                placeholder="PLACA" name="placa" />
+            <label for="floatingPlaca">PLACA</label>
+        </div>
+    </div>
+</div>
+{{-- fin de datos del vehiculo --}}
+    </div>
+</div>
+
+
+
+
+
+
+
+
             <input type="hidden" name="mov" value="{{ empty($ultimo_mov) ? 1 : $ultimo_mov->id + 1 }}">
             <input type="hidden" name="user" value="{{ auth()->user()->id }}">
             <input type="hidden" name="comandancia" class="comandancia" value="">
