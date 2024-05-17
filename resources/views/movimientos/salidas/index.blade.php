8f0e20a5da8c6c9e993b0d57675d9dfc257a19ea
Daniel <nikolazt98@gmail.com>
2024-05-16 20:29:22 -0400

    Cambie los textos a negro y el tr a blue-900 junto con el texto a blanco

--- resources/views/movimientos/salidas/index.blade.php
+++ resources/views/movimientos/salidas/index.blade.php
@@ -9,7 +9,7 @@
     @endpush
 
     <x-slot name="header">
-        <h2 class="h2 mb-2 mt-2">
+        <h2 class="h2 mb-2 mt-4 text-black">
             {{ __('Salidas Internacionales') }}
         </h2>
     </x-slot>
@@ -17,7 +17,7 @@
         <div class="col-lg-12">
             <div class="card">
                 <div class="card-header">
-                    <h2 class="h3">
+                    <h2 class="h3 text-black">
                         {{ __('Listado de Solicitudes de Salida')}}
                         <a href="{{ route('movimientos.salidas.create') }}"
                             class="inline-flex items-center justify-center px-3 py-2 bg-blue-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-500 active:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition ease-in-out duration-150 float-end"><i
@@ -28,7 +28,7 @@
                 <div class="card-body">
                     <table id="table-despacho" class="table dt-responsive table-striped nowrap w-100">
                         <thead>
-                            <tr>
+                            <tr class="bg-blue-900 text-white">
                                 <th>ID</th>
                                 <th>{{ __('Fecha Salida') }}</th>
                                 <th>{{ __('Matrícula') }}</th>
