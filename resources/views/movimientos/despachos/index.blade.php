7d3a5a2eec1d11b8333d1632d1943379aef27adb
Daniel <nikolazt98@gmail.com>
2024-05-25 14:05:51 -0400

    En el index de despachos cambie la clase bg-blue-600 a bg-azulito

--- resources/views/movimientos/despachos/index.blade.php
+++ resources/views/movimientos/despachos/index.blade.php
@@ -61,7 +61,7 @@
                                     </td>
                                     <td>
                                         <a href="{{ route('movimientos.despachos.show', $desp) }}"
-                                            class="inline-flex items-center justify-center px-3 py-2 bg-blue-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-500 active:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition ease-in-out duration-150">
+                                            class="inline-flex items-center justify-center px-3 py-2 bg-azulito border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-500 active:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition ease-in-out duration-150">
                                             <i class="uil-eye"></i>
                                         </a>
                                     </td>
