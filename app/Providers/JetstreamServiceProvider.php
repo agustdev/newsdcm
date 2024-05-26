982f513e3bfa56385b0569bae2cb74c670a5777c
Agustin Aguero <agustin.aguero@gmail.com>
2024-05-26 17:17:34 -0400

    Ajuste para los usuarios normales

--- app/Providers/JetstreamServiceProvider.php
+++ app/Providers/JetstreamServiceProvider.php
@@ -3,7 +3,12 @@
 namespace App\Providers;
 
 use App\Actions\Jetstream\DeleteUser;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Validation\ValidationException;
+use Laravel\Fortify\Fortify;
 use Laravel\Jetstream\Jetstream;
 
 class JetstreamServiceProvider extends ServiceProvider
@@ -24,6 +29,18 @@ class JetstreamServiceProvider extends ServiceProvider
         $this->configurePermissions();
 
         Jetstream::deleteUsersUsing(DeleteUser::class);
+
+        // Below code is for your customization
+        Fortify::authenticateUsing(function (Request $request) {
+            $user = User::where('email', $request->email)->first();
+
+            if ($user && Hash::check($request->password, $user->password)) {
+                if ($user->is_admin == 0) {  // it will return if status == 1
+                    return $user;
+                }
+                throw ValidationException::withMessages(['No puede iniciar sesion este usuario no posee los permisos!']);
+            }
+        });
     }
 
     /**
