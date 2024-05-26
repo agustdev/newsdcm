04908b5b19a15138cb59677d51e27ff846b92582
Agustin Aguero <agustin.aguero@gmail.com>
2024-05-26 13:02:28 -0400

    Ajuste boton cerrar en modal de fotos

--- resources/views/embarcaciones/index.blade.php
+++ resources/views/embarcaciones/index.blade.php
@@ -151,11 +151,12 @@
                 aria-labelledby="standard-modalLabel" aria-hidden="true">
                 <div class="modal-dialog modal-lg">
                     <div class="modal-content">
-                        <div class="modal-header text-center bg-blue-900">
-                            <h4 class="modal-title h3 uppercase text-white" id="standard-modalLabel">
-                               <strong> {{ __('FOTOS DE LA EMBARCACION') }}</strong></h4>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                aria-hidden="true"></button>
+                        <div class="modal-header bg-blue-900">
+                            <h5 class="modal-title h3 uppercase text-white" id="standard-modalLabel">
+                                <strong> {{ __('FOTOS DE LA EMBARCACION') }}</strong>
+                            </h5>
+                            <button type="button" class="btn-close text-white font-bold" data-bs-dismiss="modal"
+                                aria-hidden="true">X</button>
                         </div>
                         <div class="modal-body text-center">
                             <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
