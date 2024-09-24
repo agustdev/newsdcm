aa482ea7f86ac9c4ab4c1d909d3d9e863b69668d
Daniel <nikolazt98@gmail.com>
2024-09-24 18:00:52 -0400

    margenes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -649,7 +649,7 @@
             <div class="grid grid-cols-3 mx-6 justify-center mb-5">
                 <div class="flex justify-end h-12 mt-4">
                     @if (app()->getLocale() == '')
-                        <img src="{{ asset('images/do.svg') }}" alt="" class="mr-4">
+                        <img src="{{ asset('images/do.svg') }}" alt="" class="-mr-2">
                     @endif
                     @foreach (Config::get('languages') as $lang => $language)
                         @if (app()->getLocale() === $language['flag'])
