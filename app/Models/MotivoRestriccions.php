e236b242a4a7341d78631a71cc6ae4e995a58a43
Agustin Aguero <agustin.aguero@gmail.com>
2025-07-06 13:48:14 -0400

    creacion de tabla motivo_restriccions

---
+++ app/Models/MotivoRestriccions.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MotivoRestriccions extends Model
+{
+    use HasFactory;
+}
