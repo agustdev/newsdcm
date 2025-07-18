3b5544ac9bbdcb13e5b77df4027ca30babb94f47
Agustin Aguero <agustin.aguero@gmail.com>
2025-07-17 22:49:04 -0400

    Update create.blade.php

--- resources/views/movimientos/despachos/create.blade.php
+++ resources/views/movimientos/despachos/create.blade.php
@@ -167,8 +167,8 @@
                                     <option>- {{ __('Seleccione') }} -</option>
                                     @foreach ($destinos as $dest)
                                         @if ($dest->id != 13)
-                                            <option value="{{ $dest->id }}|{{ $dest->descripcion }}">
-                                                {{ $dest->descripcion }}
+                                            <option value="{{ $dest->id }}|{{ strtoupper($dest->descripcion) }}">
+                                                {{ strtoupper($dest->descripcion) }}
                                             </option>
                                         @endif
                                     @endforeach
