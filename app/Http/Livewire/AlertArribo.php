e41fa9fb8a6318c86e1ae59f7f35ba12bb6c7887
Agustin Aguero <agustin.aguero@gmail.com>
2025-07-27 21:36:20 -0400

    creacion de componente livewire para los disparadores de las notificaciones

---
+++ app/Http/Livewire/AlertArribo.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class AlertArribo extends Component
+{
+    public function render()
+    {
+        return view('livewire.alert-arribo');
+    }
+}
