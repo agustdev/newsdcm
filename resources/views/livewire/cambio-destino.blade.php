3bb59361c1562560e9666501cd9ca9c9f871c965
Agustin Aguero <agustin.aguero@gmail.com>
2025-07-28 20:52:33 -0400

    cambios menores

--- resources/views/livewire/cambio-destino.blade.php
+++ resources/views/livewire/cambio-destino.blade.php
@@ -1,7 +1,7 @@
 <div>
     <button data-id="{{ $despacho->id }}" wire:click="$set('openEdit', true)"
         class="{{ \Carbon\Carbon::parse($despacho->fecha_llegada)->isPast() ? 'hidden ' : 'inline-flex ' }}items-center justify-center px-3 py-2 bg-yellow-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-yellow-500 active:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-offset-2 transition ease-in-out duration-150 edit-desp disabled:opacity-25"
-        title="Editar"><i class="uil-edit"></i>
+        title="Cambiar destino"><i class="uil-edit"></i>
     </button>
     <x-dialog-modal wire:model="openEdit">
         <x-slot name="title">
@@ -30,7 +30,7 @@
                     @endif
                 </div>
                 <div class="mt-3 uppercase">
-                    <x-label class="text-1xl">{{ __('Nuevo Destino') }}</x-label>
+                    <x-label class="text-1xl">{{ __('Seleccionar Nuevo Destino') }}</x-label>
                     <select wire:model.defer='destino' class="form-control mb-2 rounded-md">
                         <option value="">- {{ __('Seleccione') }} -</option>
                         @foreach ($perimetros as $perimetro)
--- resources/views/movimientos/despachos/create.blade.php
+++ resources/views/movimientos/despachos/create.blade.php
@@ -363,6 +363,7 @@
                                     <option value="TURISTICO">{{ __('TURÍSTICO') }}</option>
                                     <option value="TURISTICO COMERCIAL">{{ __('TURÍSTICO COMERCIAL') }}</option>
                                     <option value="TURISTICO PRIVADO">{{ __('TURÍSTICO PRIVADO') }}</option>
+                                    {{-- <option value="TURISTICO PRIVADO">{{ __('TURÍSTICO PRIVADO') }}</option> --}}
                                     <option value="OTRO DESTINO">OTRO DESTINO</option>
                                 </select>
                                 <label style="font-size: 10px;"
